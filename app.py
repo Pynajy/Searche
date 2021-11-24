b31980cfb913c64f6727feb3e953fff2800683b0
Pynajy <vladtuzov1457@gmail.com>
2021-11-25 00:02:31 +0300

    add search in google

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from date import search_engin
-from date.urls import list_urls_1, list_urls_2
+from date.urls import list_urls_1, list_urls_2, list_urls_3, list_urls_4
 
 main_search = search_engin.Search()
 
 # main_search.search(list_urls_1)
-main_search.search(list_urls_2)
+# main_search.search_yandex(list_urls_2)
+main_search.search_google(list_urls_4)
 main_search.close_driver()
--- date/search_engin.py
+++ date/search_engin.py
@@ -3,20 +3,22 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import json
 # from date.urls import list_urls
-# import time
+import time
 # EXE_PATH это путь до ранее загруженного нами файла chromedriver.exe
+
 
 class Search:
     def __init__(self):
         EXE_PATH = r'C:/temp/chromedriver.exe'
         send_command = ('POST', '/session/$sessionId/chromium/send_command')
         self.driver = webdriver.Chrome(executable_path=EXE_PATH)
-        self.driver.set_window_size(1400,1500) 
+        # self.driver = webdriver.Chrome()
+        self.driver.set_window_size(1400, 1500)
         self.driver.command_executor._commands['SEND_COMMAND'] = send_command
         # time.sleep(200)
         # self.driver.execute('SEND_COMMAND', dict(cmd='Network.clearBrowserCookies', params={}))
 
-    def search(self, url_list):
+    def search_yandex(self, url_list):
         results_name_sites = {
             url_list['name']: {
                 'good': {},
@@ -24,7 +26,8 @@ class Search:
             }
         }
         for urls in url_list['urls']:
-            self.driver.execute('SEND_COMMAND', dict(cmd='Network.clearBrowserCookies', params={}))
+            self.driver.execute('SEND_COMMAND', dict(
+                cmd='Network.clearBrowserCookies', params={}))
             urls = urls.replace(' ', '+')
             if 'ереповец' not in urls:
                 url = f'https://yandex.ru/search/?text={urls}+череповец'
@@ -45,14 +48,52 @@ class Search:
             # for ind in url_list['top']:
                 # print(ind)
             if list_cheker_urls[0] not in url_list['top'][0]:
-                results_name_sites[url_list['name']]['bad'][url] = list_cheker_urls
+                results_name_sites[url_list['name']
+                                   ]['bad'][url] = list_cheker_urls
                 self.driver.save_screenshot(f'screenshot/{urls}.png')
             else:
                 # self.driver.save_screenshot(f'screenshot/{urls}.png')
-                results_name_sites[url_list['name']]['good'][url] = list_cheker_urls
+                results_name_sites[url_list['name']
+                                   ]['good'][url] = list_cheker_urls
         with open('results.json', 'w', encoding='utf-8') as f:
             json.dump(results_name_sites, f, ensure_ascii=False, indent=4)
-    
+
+    def search_google(self, list_urls):
+        for url in list_urls['urls']:
+            list_cheker_urls = []
+            # url = url.replace("+", ' ').split().replace(' ', '+')
+            url = url.replace(" ", '')
+            if url[0] == "+":
+                url = url[1::]
+            if 'ереповец' not in url:
+                url = url + '+череповец'
+            # print(url)
+
+            res_url = f'https://www.google.com/search?q={url}'
+            self.driver.execute('SEND_COMMAND', dict(
+                    cmd='Network.clearBrowserCookies', params={}))
+            self.driver.get(res_url)
+            reklama = self.driver.find_element_by_id('taw')
+            if reklama:
+                res = self.driver.find_element_by_xpath("//span[@role='text']").text
+                list_cheker_urls.append(res)
+            res = self.driver.find_elements_by_tag_name('cite')
+            for i in res:
+                list_cheker_urls.append(i.text)
+            list_cheker_urls = [i for i in list_cheker_urls if i != '']
+            # print(url)
+            # print(list_cheker_urls)
+            flag = False
+            for i in list_urls['top'][0]:
+                if i not in list_cheker_urls[0]:
+                    pass
+                else:
+                    flag = True
+            if flag == False:
+                self.driver.save_screenshot(f'screenshot/{url}.png')
+                print(f"{res_url} Запрос не соотвецтвует требованиям")
+
+        # time.sleep(200)
+
     def close_driver(self):
         self.driver.close()
-
